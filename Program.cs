e0726d9e33c857889be8544d7959aebcaf31c53f
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-10-18 15:28:16 +0530

    Add files via upload
    Added UC1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using LineComparision.cs;
+
+namespace Line_Comparision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to Line Comparison Computation");
+            //LengthOfLine.Length();
+            //LengthEqual.Length();
+            UserCase1LengthOfLine.Length();
+        }
+    }
+}
